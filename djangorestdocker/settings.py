1e23f64c8c31eae1915b9c412185bffcd022d1eb
Luka <lgurjidze@gmail.com>
2024-06-06 15:30:30 +0400

    dockerize app

--- djangorestdocker/settings.py
+++ djangorestdocker/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'djangorestdocker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres'
     }
 }
 
